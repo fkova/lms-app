c4a7374bab62b73c47a5a0cf043e292f01712529
Feri <fkovacs088@gmail.com>
2026-06-24 15:16:35 +0200

    init tabs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,54 @@ import "@/global.css";
 
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+import { ClerkProvider, useAuth } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+import { ActivityIndicator, View, StyleSheet, Button, Modal } from "react-native";
+import { AuthView, UserButton } from '@clerk/expo/native'
+import React, { useState } from "react";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
 }
+
+function RootStack() {
+  const { isSignedIn, isLoaded } = useAuth({ treatPendingAsSignedOut: false })
+  const [isAuthOpen, setIsAuthOpen] = useState(false)
+
+  if (!isLoaded) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+
+  if (!isSignedIn) {
+    return <AuthView onDismiss={() => setIsAuthOpen(false)} />
+  }
+
+  return <Stack />
+}
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <RootStack />
+    </ClerkProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
